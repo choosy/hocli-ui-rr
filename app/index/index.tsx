169f96538c5c353f35ceea5379ef72e69b95a874
choosy <stefan.horomnea@daon.com>
2026-04-04 16:09:45 +0300

    cleanup. layout skeleton for index

---
+++ app/index/index.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/components/index/index";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Your Ethereum clothing store" },
+    {
+      name: "description",
+      content: "Welcome to Hocli, your web3 clothing store!",
+    },
+  ];
+}
+
+export default function Index() {
+  return <div>ha</div>;
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,5 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout("./index/layout.tsx", [index("./index/index.tsx")]),
+] satisfies RouteConfig;
