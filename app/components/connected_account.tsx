99943d3002bbf493c51800c19f855693624ec8c7
choosy <stefan.horomnea@daon.com>
2026-04-19 19:49:32 +0300

    wip

--- app/components/connected_account.tsx
+++ app/components/connected_account.tsx
@@ -1,9 +1,22 @@
+import { useConnect, useConnection } from "wagmi";
+import { injected } from "wagmi/connectors";
+
 import { Button } from "./button";
 
 export function ConnectedAccount() {
-  return (
-    <Button className="hidden lg:block lg:px-8 lg:py-1.5">
-      Connect Wallet
-    </Button>
-  );
+  let connect = useConnect();
+  let connection = useConnection();
+
+  if (connection.isConnected) {
+    return <div>{connection.address} </div>;
+  } else {
+    return (
+      <Button
+        className="hidden lg:block lg:px-8 lg:py-1.5"
+        onClick={() => connect.mutate({ connector: injected() })}
+      >
+        Connect Wallet
+      </Button>
+    );
+  }
 }
