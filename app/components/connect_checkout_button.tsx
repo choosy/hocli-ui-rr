068d287e544a0d5c0b0176ecb238fb0c0ac7c10f
choosy <stefan.horomnea@daon.com>
2026-04-19 21:27:36 +0300

    fixes

--- app/components/connect_checkout_button.tsx
+++ app/components/connect_checkout_button.tsx
@@ -1,34 +1,45 @@
 import { useNavigate } from "react-router";
-import { useAccount } from "wagmi";
-
-import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useConnect, useConnection } from "wagmi";
+import { injected } from "wagmi/connectors";
 
 import { Button } from "./button";
 
 import { useCart } from "app/lib/cart";
 
-export function ConnectCheckoutButton({ type, ...props }) {
+type ConnectCheckoutButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function ConnectCheckoutButton({
+  type,
+  ...props
+}: ConnectCheckoutButtonProps) {
   const navigate = useNavigate();
   const { closeCart } = useCart();
-  const account = useAccount();
+  const connection = useConnection();
+  const connect = useConnect();
 
-  const { openConnectModal } = useConnectModal();
+  let buttonText: string;
+  let buttonAction: React.MouseEventHandler<HTMLButtonElement>;
 
-  let buttonText, buttonAction;
-
-  if (account.isConnected) {
+  if (connection.isConnected) {
     buttonText = "CHECKOUT";
     buttonAction = (e) => {
       closeCart(e);
       navigate("/checkout");
     };
   } else {
-    buttonText = "CONNECT WALLET";
-    buttonAction = () => openConnectModal();
+    buttonText = connect.isPending ? "CONNECTING..." : "CONNECT WALLET";
+    buttonAction = () => connect.mutate({ connector: injected() });
   }
+
   return (
     <div>
-      <Button type={type} {...props} onClick={buttonAction} className="w-full">
+      <Button
+        type={type}
+        {...props}
+        onClick={buttonAction}
+        disabled={connect.isPending}
+        className="w-full"
+      >
         {buttonText}
       </Button>
     </div>
