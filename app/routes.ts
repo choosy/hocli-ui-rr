100c23eae56d93e1c26b0bbba39e08fa39a113ae
choosy <stefan.horomnea@daon.com>
2026-04-23 00:01:08 +0300

    fixes

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,5 @@
 import { type RouteConfig, index, layout } from "@react-router/dev/routes";
 
 export default [
-  layout("./index/layout.tsx", [index("./index/index.tsx")]),
+  layout("./pages/index/layout.tsx", [index("./pages/index/index.tsx")]),
 ] satisfies RouteConfig;
